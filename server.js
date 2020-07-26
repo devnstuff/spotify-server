321f4ab2c35729020fc51d4382189fd9c51f4128
Povilas Plauska <povilasplauska@Povilass-iMac.local>
2020-07-26 18:37:32 +0300

    update

--- server.js
+++ server.js
@@ -4,49 +4,51 @@ const querystring = require('querystring');
 require('dotenv').config();
 const app = express();
 
-const frontend_uri = process.env.FRONTEND_URI || 'http://localhost:8080';
+if (process.env.NODE_ENV === 'development') {
+	const frontend_uri = process.env.FRONTEND_URI || 'http://localhost:8080';
 
-app.get('/login', (req, res) => {
-	res.redirect(
-		'https://accounts.spotify.com/authorize?' +
-			querystring.stringify({
-				response_type: 'code',
-				client_id: process.env.CLIENT_ID,
-				scope: process.env.SCOPES,
-				redirect_uri: process.env.REDIRECT_URI,
-			})
-	);
-});
-
-app.get('/callback', (req, res) => {
-	const code = req.query.code || null;
-	const authOptions = {
-		url: 'https://accounts.spotify.com/api/token',
-		form: {
-			code,
-			redirect_uri: process.env.REDIRECT_URI,
-			grant_type: 'authorization_code',
-		},
-		headers: {
-			Authorization: `Basic ${new Buffer.from(
-				`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
-			).toString('base64')}`,
-		},
-		json: true,
-	};
-	request.post(authOptions, (error, response, body) => {
-		if (!error && response.statusCode === 200) {
-			const access_token = body.access_token;
-			const refresh_token = body.refresh_token;
-
-			res.redirect(
-				frontend_uri + '?access_token=' + access_token + '&refresh_token=' + refresh_token
-			);
-		} else {
-			console.log('Error, invalid token');
-		}
+	app.get('/login', (req, res) => {
+		res.redirect(
+			'https://accounts.spotify.com/authorize?' +
+				querystring.stringify({
+					response_type: 'code',
+					client_id: process.env.CLIENT_ID,
+					scope: process.env.SCOPES,
+					redirect_uri: process.env.REDIRECT_URI,
+				})
+		);
 	});
-});
+
+	app.get('/callback', (req, res) => {
+		const code = req.query.code || null;
+		const authOptions = {
+			url: 'https://accounts.spotify.com/api/token',
+			form: {
+				code,
+				redirect_uri: process.env.REDIRECT_URI,
+				grant_type: 'authorization_code',
+			},
+			headers: {
+				Authorization: `Basic ${new Buffer.from(
+					`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
+				).toString('base64')}`,
+			},
+			json: true,
+		};
+		request.post(authOptions, (error, response, body) => {
+			if (!error && response.statusCode === 200) {
+				const access_token = body.access_token;
+				const refresh_token = body.refresh_token;
+
+				res.redirect(
+					frontend_uri + '?access_token=' + access_token + '&refresh_token=' + refresh_token
+				);
+			} else {
+				console.log('Error, invalid token');
+			}
+		});
+	});
+}
 
 app.get('/refresh_token', (req, res) => {
 	const refresh_token = req.query.refresh_token;
